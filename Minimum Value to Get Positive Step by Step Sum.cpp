5fde934fe38ebe8e401f9a5d7e3b1307d2ebf73c
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-30 09:37:46 +0530

    Minimum Value to Get Positive Step by Step Sum.cpp

--- Minimum Value to Get Positive Step by Step Sum.cpp
+++ Minimum Value to Get Positive Step by Step Sum.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int minStartValue(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefixSum(n, 0);
+        vector<int> prefixSum(n+1, 0);
         prefixSum[0] = nums[0];
         for (int i = 1; i < n; ++i) {
             prefixSum[i] = prefixSum[i - 1] + nums[i];
