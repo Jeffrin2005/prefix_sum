cf8bb829ed3d5fa80d9c2d53eba6f3609d7d3794
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-31 08:50:44 +0530

    Maximum XOR for Each Query.cpp

---
+++ Maximum XOR for Each Query.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+// time comp =o(n) , space comp = o(n)
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+        vector<int> answer(n);
+        int maxXor = (1 << maximumBit) - 1; 
+        vector<int>prefixXor(n + 1);
+        prefixXor[0] = nums[0];
+        for(int i = 1; i < n;i++){
+            prefixXor[i] = prefixXor[i - 1] ^ nums[i];
+        }
+        for(int i = 0; i < n;i++){
+            answer[i] = maxXor ^ prefixXor[n - 1 - i]; 
+        }
+
+        return answer;
+    }
+};
